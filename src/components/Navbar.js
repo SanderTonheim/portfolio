f6f6044c0f91560eb6d03addb3f4d8245d6e5fcc
Sander <sandert.kodehode@gmail.com>
2022-08-11 11:20:53 +0200

    start

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { StyledNavbar } from './styled/Navbar.styled'
+
+export function Navbar() {
+	return (
+		<StyledNavbar>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</StyledNavbar>
+	)
+}
---
+++ src/components/styled/Navbar.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const StyledNavbar = styled.div`
+	background-color: #333;
+	color: #fff;
+
+	ul {
+		list-style-type: none;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+	}
+`
