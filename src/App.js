0af7fd87483733f29d15db7bde18203a85e7c7c8
Sander <sandert.kodehode@gmail.com>
2022-08-11 13:47:15 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import { Navbar } from './components/Navbar'
+import { GlobalStyles } from './components/GlobalStyles'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Landingpage from './pages/LandingPage'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+
 function App() {
 	return (
-		<div className='App'>
-			<Navbar />
-		</div>
+		<Router>
+			<div className='App'>
+				<GlobalStyles />
+				<div className='nav'>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/about'>About</Link>
+						</li>
+						<li>
+							<Link to='/contact'>Contact</Link>
+						</li>
+					</ul>
+				</div>
+				<Routes>
+					<Route exact path='/' element={<Landingpage />}></Route>
+					<Route exact path='/about' element={<AboutPage />}></Route>
+					<Route exact path='/contact' element={<ContactPage />}></Route>
+				</Routes>
+			</div>
+		</Router>
 	)
 }
 
--- src/components/styled/Navbar.styled.js
+++ src/components/styled/Navbar.styled.js
@@ -3,11 +3,22 @@ import styled from 'styled-components'
 export const StyledNavbar = styled.div`
 	background-color: #333;
 	color: #fff;
+	width: 100%;
+	height: 5rem;
+	padding: 0px;
 
 	ul {
+		padding: 0;
+		width: auto;
+		height: 100%;
 		list-style-type: none;
+		margin: 0;
 		display: flex;
-		flex-direction: row;
+		align-items: center;
 		justify-content: flex-end;
+		padding-right: 1rem;
+	}
+	li {
+		padding: 0 1rem;
 	}
 `
