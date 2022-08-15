05454e6fff576fc473a773c893ca57f78789eeaa
Sander <sandert.kodehode@gmail.com>
2022-08-15 13:20:03 +0200

    Updates

--- src/components/MainPic.js
+++
@@ -1,5 +0,0 @@
-import Man from '../images/Man.jpg'
-
-export function MainPic() {
-	return <img src={Man} alt='Man' />
-}
--- src/pages/ContactPage.js
+++
@@ -1,5 +0,0 @@
-function ContactPage() {
-	return <h1>contact page</h1>
-}
-
-export default ContactPage
