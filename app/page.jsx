5199acd6e2b2d6008b4efb5ed47c2c8da90a9f18
Sander Tonheim <sandert.kodehode@gmail.com>
2023-09-26 16:13:17 +0200

    created page

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<main>
+			<h1>Sander Tonheim</h1>
+			<ul>
+				<li>Alder: 24år</li>
+				<li>Bosted: Bergen</li>
+				<li>Utdannelse: fagrev i IKT</li>
+			</ul>
+		</main>
+	)
+}
