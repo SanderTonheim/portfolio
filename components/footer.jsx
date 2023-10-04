3e207aa5a471160f5dbe3ac7a2613a35992b08f5
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-05 01:50:15 +0200

    styling

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,7 +2,7 @@ import github from '@/assets/footer/github.svg'
 import Image from 'next/image'
 export default function Footer() {
 	return (
-		<section className=' bg-slate-700 h-20 flex px-5 lg:justify-center'>
+		<section className=' bg-black h-20 flex px-5 lg:justify-center'>
 			<div className=' w-full flex justify-between items-center lg:max-w-7xl '>
 				<p>Sander Tonheim</p>
 				<Image
