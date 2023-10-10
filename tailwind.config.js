69c57c07a737980d16dc8100b7b561af5121f16b
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-10 16:32:40 +0200

    added config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}', './assets/**/*.{png,jpg,jpeg,svg}'],
-	theme: {
-		extend: {
-			backgroundImage: {},
-		},
-	},
-	plugins: [],
-}
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./assets/**/*.{png,jpg,jpeg,svg}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {},
+    },
+  },
+
+  plugins: [require("tailwindcss-animated")],
+};
