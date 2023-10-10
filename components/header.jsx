c9cc36a6cde78c220589740745d2cc0fd2f8e46a
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-10 16:35:47 +0200

    styling

--- components/header.jsx
+++ components/header.jsx
@@ -1,18 +1,15 @@
-import Image from 'next/image'
-import image from '@/assets/heroImage.png'
+import Image from "next/image";
+import image from "@/assets/heroImage.png";
 
 export default function Header() {
-	return (
-		<section className='CONTAINER text-6xl flex flex-col text-white md:flex-row-reverse md:justify-between lg:w-full lg:max-w-4xl lg:self-center xl:max-w-6xl xl:flex-col xl:items-center '>
-			<div className='TEXT bg-gradient-to-t from-white/0 to-slate-100/90 bg-clip-text text-transparent self-center max-w-sm flex flex-wrap w-full p-5 md:self-start md:pl-0 md:text-[4.3rem] xl:text-9xl xl:max-w-4xl xl:self-center xl:p-0'>
-				<h1 className=''>Sander</h1>
-				<h1 className='flex justify-end w-full xl:-mt-5 xl:-ml-20'>Tonheim</h1>
-			</div>
+  return (
+    <section className="CONTAINER flex flex-col text-6xl text-white md:flex-row-reverse md:justify-between lg:w-full lg:max-w-4xl lg:self-center xl:max-w-6xl xl:flex-col xl:items-center  ">
+      <div className="TEXT  flex w-full max-w-sm animate-fade-up flex-wrap self-center bg-gradient-to-t from-white/0 to-slate-100/90 bg-clip-text p-5 text-transparent md:self-start md:pl-0 md:text-[4.3rem] xl:max-w-4xl xl:self-center xl:p-0 xl:text-9xl">
+        <h1 className="">Sander</h1>
+        <h1 className="flex w-full justify-end xl:-ml-20 xl:-mt-5">Tonheim</h1>
+      </div>
 
-			<Image
-				src={image}
-				className='xl:-mt-20 w-80'
-			/>
-		</section>
-	)
+      <Image src={image} className="w-80 xl:-mt-20" />
+    </section>
+  );
 }
--- components/projects.jsx
+++ components/projects.jsx
@@ -1,62 +1,60 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import bfnr from '@/assets/bfnr.png'
-import foks from '@/assets/foks.svg'
-import hawii from '@/assets/Hawii.svg'
-import jobbjakt from '@/assets/jobbjakt.svg'
+import Image from "next/image";
+import Link from "next/link";
+import bfnr from "@/assets/bfnr.png";
+import foks from "@/assets/foks.svg";
+import hawii from "@/assets/Hawii.svg";
+import jobbjakt from "@/assets/jobbjakt.svg";
 
 export default function Projects() {
-	return (
-		<section className='CONTAINER bg-[url("../assets/bg.jpg")] '>
-			<div className='CONTENT py-10 px-4 flex flex-col gap-6 md:items-center '>
-				<h2 className='text-[#8a8a8a] md:max-w-2xl md:w-full xl:max-w-7xl'>Prosjekter</h2>
-				<div className='CARDS flex flex-wrap gap-8 justify-center xl:max-w-7xl xl:justify-start'>
-					<Card
-						image={bfnr}
-						link={'https://medlemsliste.bfnr.no/'}
-						projectName={'Bedriftregister'}
-						projectFor={'Bjørnafjorden Næringsråd'}
-					/>
-					<Card
-						image={foks}
-						link={'https://www.obligatorisk.no/'}
-						projectName={'Oppmøteprosent kalkulator'}
-						projectFor={'Foks AS'}
-					/>
-					<Card
-						image={hawii}
-						link={'https://www.hawii.no/bedrift'}
-						projectName={'Hawii Analyse'}
-						projectFor={'Kodeverket'}
-					/>
-					<Card
-						image={jobbjakt}
-						link={'https://www.jobbjakt.no/'}
-						projectName={'Jobbjakt hjemmeside'}
-						projectFor={'Kodeverket'}
-					/>
-				</div>
-			</div>
-		</section>
-	)
+  return (
+    <section className='CONTAINER bg-[url("../assets/bg.jpg")] '>
+      <div className="CONTENT flex flex-col gap-6 px-4 py-10 md:items-center ">
+        <h2 className="text-[#8a8a8a] md:w-full md:max-w-2xl xl:max-w-7xl">
+          Prosjekter
+        </h2>
+        <div className="CARDS flex flex-wrap justify-center gap-8 xl:max-w-7xl xl:justify-start">
+          <Card
+            image={bfnr}
+            link={"https://medlemsliste.bfnr.no/"}
+            projectName={"Bedriftregister"}
+            projectFor={"Bjørnafjorden Næringsråd"}
+          />
+          <Card
+            image={foks}
+            link={"https://www.obligatorisk.no/"}
+            projectName={"Oppmøteprosent kalkulator"}
+            projectFor={"Foks AS"}
+          />
+          <Card
+            image={hawii}
+            link={"https://www.hawii.no/bedrift"}
+            projectName={"Hawii Analyse"}
+            projectFor={"Kodeverket"}
+          />
+          <Card
+            image={jobbjakt}
+            link={"https://www.jobbjakt.no/"}
+            projectName={"Jobbjakt hjemmeside"}
+            projectFor={"Kodeverket"}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export const Card = ({ image, link, projectName, projectFor }) => {
-	return (
-		<div className=' min-w-[250px] max-w-[288px]   '>
-			<Link href={link}>
-				<div className='h-60 flex justify-center items-center p-7 bg-[#292929]/5 backdrop-blur-[6px]'>
-					<Image
-						src={image}
-						className=' m-auto'
-						alt='logo'
-					/>
-				</div>
-				<div className='bg-[#1a1a1a] h-20 px-3  flex flex-col justify-center '>
-					<p className='text-white'>{projectName}</p>
-					<p className='text-white opacity-50'>{projectFor}</p>
-				</div>
-			</Link>
-		</div>
-	)
-}
+  return (
+    <div className=" min-w-[250px] max-w-[288px]   ">
+      <Link href={link}>
+        <div className="flex h-60 items-center justify-center bg-[#292929]/5 p-7 backdrop-blur-[6px]">
+          <Image src={image} className=" m-auto" alt="logo" />
+        </div>
+        <div className="flex h-20 flex-col  justify-center bg-[#1a1a1a] px-3 ">
+          <p className="text-white">{projectName}</p>
+          <p className="text-white opacity-50">{projectFor}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
