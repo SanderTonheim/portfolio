eae1bfd40b33293045495cf1cfcd6f103bce7f04
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-15 20:30:13 +0200

    added background animation

--- components/about.jsx
+++ components/about.jsx
@@ -1,25 +1,26 @@
 "use client";
 import Lottie from "lottie-react";
 import animation from "@/assets/animations/about.json";
+import test from "@/assets/animations/test-animation.json";
 import { useRef } from "react";
 export default function About() {
   const animationRef = useRef();
   return (
-    <section className="bg-[#1A1A1A] px-4 py-8 text-white  ">
-      <p className="md:mx-auto md:max-w-lg">
+    <section className=" flex items-center justify-center overflow-hidden bg-[#1A1A1A] text-white  ">
+      <Lottie
+        animationData={test}
+        // lottieRef={animationRef}
+        // onComplete={() => {
+        //   animationRef.current?.goToAndPlay(20, true);
+        // }}
+        className=" relative flex max-w-md items-center  justify-center blur-sm"
+        loop={true}
+      />
+      <p className=" absolute z-20 max-w-md px-10 md:relative md:mx-auto md:max-w-lg md:p-10">
         Mitt navn er Sander. Jeg er en front-end utvikler fra Bergen. Målet mitt
         er å bidra til en <strong>bedre og lettere fremtid</strong> med hjelp av
         dagens teknologi.
       </p>
-      <Lottie
-        animationData={animation}
-        lottieRef={animationRef}
-        // onComplete={() => {
-        //   animationRef.current?.goToAndPlay(20, true);
-        // }}
-        className="w-full md:hidden"
-        loop={false}
-      />
     </section>
   );
 }
