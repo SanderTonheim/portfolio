c032400c4736802010a265591a6a11eaff66a48a
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-15 21:19:19 +0200

    bg color change, added text padding

--- components/header.jsx
+++ components/header.jsx
@@ -3,8 +3,8 @@ import image from "@/assets/heroImage.png";
 
 export default function Header() {
   return (
-    <section className="CONTAINER flex flex-col text-6xl text-white md:flex-row-reverse md:justify-between lg:w-full lg:max-w-4xl lg:self-center xl:max-w-6xl xl:flex-col xl:items-center  ">
-      <div className="TEXT  flex w-full max-w-sm animate-fade-up flex-wrap self-center bg-gradient-to-t from-white/0 to-slate-100/90 bg-clip-text p-5 text-transparent md:self-start md:pl-0 md:text-[4.3rem] xl:max-w-4xl xl:self-center xl:p-0 xl:text-9xl">
+    <section className="CONTAINER  flex flex-col bg-gradient-to-t from-[#161616] to-black pt-4 text-6xl text-white md:flex-row-reverse md:justify-between lg:w-full lg:max-w-4xl lg:self-center xl:max-w-6xl xl:flex-col xl:items-center ">
+      <div className="TEXT  flex w-full max-w-sm animate-fade-up flex-wrap self-center bg-gradient-to-t from-white/0 to-slate-100/90 bg-clip-text px-5 text-transparent md:self-start md:pl-0 md:text-[4.3rem] xl:max-w-4xl xl:self-center xl:p-0 xl:text-9xl">
         <h1 className="">Sander</h1>
         <h1 className="flex w-full justify-end xl:-ml-20 xl:-mt-5">Tonheim</h1>
       </div>
