6adb709a873e4db6b609ad0cdf5b6769996573c2
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-17 16:29:54 +0200

    formating

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,11 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./assets/**/*.{png,jpg,jpeg,svg}",
+    "./assets/**/*.{png,jpg,jpeg,svg,json}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {},
-    },
+    extend: {},
+    backgroundImage: {},
   },
 
   plugins: [require("tailwindcss-animated")],
