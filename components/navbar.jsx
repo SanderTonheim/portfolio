47e7e16569cf5a5e93a161c774dc01e25ee815ab
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-17 16:34:30 +0200

    rewrote component to slide in animation navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,65 +1,81 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
-import menuIcon from '@/assets/menu.svg'
-import logo from '@/assets/logo.svg'
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import menuIcon from "@/assets/menu.svg";
+import logo from "@/assets/logo.svg";
+import close from "@/assets/close.png";
 
 export default function Navbar() {
-	const [open, setOpen] = useState(false)
-	const toggle = () => {
-		{
-			setOpen(!open)
-		}
-	}
+  const [showSidebar, setShowSidebar] = useState(false);
 
-	const refOne = useRef()
-	const handleClickOutSide = (e) => {
-		if (refOne.current.contains(e.target)) {
-		} else {
-			setOpen(false)
-		}
-	}
-
-	useEffect(() => {
-		document.addEventListener('click', handleClickOutSide)
-	}, [])
-
-	return (
-		<nav className=' h-fit sticky bg-[#010101]'>
-			<div className='flex justify-between items-center px-6 py-4 max-w-7xl mx-auto xl:px-0'>
-				<Image
-					src={logo}
-					className='w-9'
-					alt='menu icon'
-				/>
-				<div
-					ref={refOne}
-					className='md:hidden'>
-					<Image
-						src={menuIcon}
-						className='w-9'
-						onClick={toggle}
-						alt='menu icon'
-					/>
-				</div>
-
-				<ul className='hidden md:flex md:items-start md:w-fit md:gap-4 md:py-2 '>
-					<li>Hjem</li>
-					<li>Forside</li>
-					<li>Medlemsliste</li>
-				</ul>
-			</div>
-
-			{open ? (
-				<div className=' bg-white flex justify-center fixed w-full md:hidden'>
-					<ul className='flex flex-col items-start w-fit gap-1 py-2 '>
-						<li>Hjem</li>
-						<li>Prosjekter</li>
-						<li>Om</li>
-					</ul>
-				</div>
-			) : null}
-		</nav>
-	)
+  return (
+    <>
+      <nav className=" sticky left-0 top-0 z-20 mx-auto flex h-20 w-full max-w-7xl items-center  justify-between bg-[#010101] px-6 py-4 xl:px-0">
+        {/* logo */}
+        <Image src={logo} className="w-9" alt="menu icon" />
+        <Image
+          src={menuIcon}
+          onClick={() => setShowSidebar(!showSidebar)}
+          alt="menu icon"
+        />
+        <section
+          className={`SLIDE-IN-CONTAINER absolute right-0 top-0 z-40 flex min-h-screen w-full flex-col bg-black  text-white duration-500 ease-in-out ${
+            showSidebar ? "translate-x-0 " : "translate-x-full"
+          }`}
+        >
+          <div className="MENU-CLOSE mx-auto flex h-20 w-full items-center justify-between px-6 py-4 ">
+            <h1 className="text-lg">Menu</h1>
+            <Image
+              src={close}
+              onClick={() => setShowSidebar(!showSidebar)}
+              alt="menu icon"
+              className="w-6 cursor-pointer"
+            />
+          </div>
+          <ul
+            className={`LIST flex flex-col items-center justify-center gap-10 self-center text-xl font-bold ${
+              showSidebar
+                ? "animate-fade-down animate-delay-300"
+                : "animate-fade-up"
+            }`}
+          >
+            <li>Topp</li>
+            <li>Prosjekter</li>
+            <li>Github</li>
+          </ul>
+        </section>
+      </nav>
+    </>
+  );
 }
+//   const [open, setOpen] = useState(false);
+
+//   return (
+//     <nav className=" sticky left-0 top-0 z-20 h-fit bg-[#010101]">
+//       <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4 xl:px-0">
+//         <div className="w-9">
+//         </div>
+//       </div>
+//       <ul
+//         className={
+//           open
+//             ? " animate-flip-down "
+//             : " animate-flip-up  " +
+//               "relative left-0 top-0 z-50 flex w-full flex-col items-center bg-white "
+//         }
+//       >
+//         <li>Hjem</li>
+//         <li>Prosjekter</li>
+//         <li>Om</li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// {
+//   /* <ul className="hidden md:flex md:w-fit md:items-start md:gap-4 md:py-2 ">
+//     <li>Hjem</li>
+//     <li>Forside</li>
+//     <li>Medlemsliste</li>
+//   </ul> */
