3a7158ce393241d16112bb13d5e4cebe02fd71dd
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-17 12:42:20 +0200

    refactored component to avoid unnecessary use of 'use client'

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
 import bfnr from "@/assets/bfnr.png";
 import foks from "@/assets/foks.svg";
 import hawii from "@/assets/Hawii.svg";
 import jobbjakt from "@/assets/jobbjakt.svg";
+import { Card } from "./card";
 
 export default function Projects() {
   return (
@@ -12,7 +11,7 @@ export default function Projects() {
         <h2 className="text-[#8a8a8a] md:w-full md:max-w-2xl xl:max-w-7xl">
           Prosjekter
         </h2>
-        <div className="CARDS flex flex-wrap justify-center gap-8 xl:max-w-7xl xl:justify-start">
+        <div className="CARDS flex animate-fade-up flex-wrap justify-center gap-8 xl:max-w-7xl xl:justify-start">
           <Card
             image={bfnr}
             link={"https://medlemsliste.bfnr.no/"}
@@ -42,19 +41,3 @@ export default function Projects() {
     </section>
   );
 }
-
-export const Card = ({ image, link, projectName, projectFor }) => {
-  return (
-    <div className=" min-w-[250px] max-w-[288px]   ">
-      <Link href={link}>
-        <div className="flex h-60 items-center justify-center bg-[#292929]/5 p-7 backdrop-blur-[6px]">
-          <Image src={image} className=" m-auto" alt="logo" />
-        </div>
-        <div className="flex h-20 flex-col  justify-center bg-[#1a1a1a] px-3 ">
-          <p className="text-white">{projectName}</p>
-          <p className="text-white opacity-50">{projectFor}</p>
-        </div>
-      </Link>
-    </div>
-  );
-};
