477da928446339d7ba3ec1e4422b9fb10eb29a64
Sander Tonheim <sandert.kodehode@gmail.com>
2023-10-24 16:04:12 +0200

    added email and link to github

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,16 +1,19 @@
-import github from '@/assets/footer/github.svg'
-import Image from 'next/image'
+import github from "@/assets/footer/github.svg";
+import Image from "next/image";
+import Link from "next/link";
 export default function Footer() {
-	return (
-		<section className=' bg-black h-20 flex px-5 lg:justify-center'>
-			<div className=' w-full flex justify-between items-center lg:max-w-7xl '>
-				<p>Sander Tonheim</p>
-				<Image
-					src={github}
-					height={40}
-					width={40}
-				/>
-			</div>
-		</section>
-	)
+  return (
+    <section className=" flex h-28 bg-black px-5 lg:justify-center">
+      <div className=" flex w-full items-center justify-between text-white lg:max-w-7xl ">
+				<div className="flex flex-col">
+
+        <p>Sander Tonheim</p>
+        <p>Email: tonheims98@gmail.com</p>
+				</div>
+        <Link href={"https://github.com/SanderTonheim"} target="_blank">
+          <Image src={github} height={40} width={40} />
+        </Link>
+      </div>
+    </section>
+  );
 }
